52921370c27849d6e0a86026c14be8f4c734e13f
Ndiane <neardiop@gmail.com>
2025-11-25 23:14:35 +0000

    feat: add components structure (todos, persons, modals)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoModalComponent } from './components/todo-modal/todo-modal.component';
+import { PersonsComponent } from './components/persons/persons.component';
+import { PersonModalComponent } from './components/person-modal/person-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoModalComponent,
+    PersonsComponent,
+    PersonModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/person-modal/person-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-modal',
+  templateUrl: './person-modal.component.html',
+  styleUrls: ['./person-modal.component.css']
+})
+export class PersonModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
